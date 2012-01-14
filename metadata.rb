5864f5fb1b79c0d64ec95cb491a592b47154b447
Jesse Campbell <hikeit@gmail.com>
2012-01-13 19:14:49 -0500

    authconfig first test

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures authconfig"
+maintainer       "Jesse Campbell"
+maintainer_email "hikeit@gmail.com"
+license          "Apache License"
+description      "Configures authconfig"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
