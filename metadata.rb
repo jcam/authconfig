4c31b8a1a57e7cea0a53c88d109ca6566c7ff397
Thom May <tmay@expedia.com>
2013-05-29 10:50:56 +0100

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "hikeit@gmail.com"
 license          "Apache License"
 description      "Configures authconfig"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.3"
+version          "0.3.1"
