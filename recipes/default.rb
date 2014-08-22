b06459e459369de65440479ea33eaed8d02b82e7
Nico Kadel-Garcia <nkadelgarcia-consultant@scholastic.com>
2014-08-22 16:57:32 -0400

    Correct start_command for sssd.

--- recipes/default.rb
+++ recipes/default.rb
@@ -83,7 +83,7 @@ if node[:platform_version].to_i == 6
 		# Avoid starting or restarting sssd if disabled,
 		# especially when kerberos is enabled, and ldap not
 		restart_command "/sbin/chkconfig sssd | grep -v :on || /sbin/service sssd restart"
-		start_command "/sbin/chkconfig sssd | grep -v :on || /sbin/service sssd restart"
+		start_command "/sbin/chkconfig sssd | grep -v :on || /sbin/service sssd start"
 	end
 
 	execute "clean_sss_db" do
