72a2e3beabfdc5d1834b0ad8f66e24b5b8adf957
Evan Felix <evan.felix@pnnl.gov>
2015-12-28 19:30:30 -0800

    more rhel7 helps

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "hikeit@gmail.com"
 license          "Apache License"
 description      "Configures authconfig"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.5"
+version          "1.1.6"
